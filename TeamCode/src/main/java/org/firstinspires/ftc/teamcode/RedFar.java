a6500ce3cb756d183729d63da40b99530c0e4cf6
Brandon <brandon@brandontsai.com>
2016-12-29 15:52:18 -0500

    added the driveUsingGyro method from last year --Austin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFar.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.ftccommon.DbgLog;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import FlamingPhoenix.Direction;
+import FlamingPhoenix.MecanumDriveTrain;
+import FlamingPhoenix.MyUtility;
+import FlamingPhoenix.TurnDirection;
+
+/**
+ * Created by brandon on 12/29/2016.
+ */
+
+public class RedFar extends LinearOpMode {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    ModernRoboticsI2cGyro gyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+
+    private VuforiaLocalizer vuforia;
+    VuforiaTrackables tracker;
+
+    DcMotor shooter;
+
+    Servo stopper;
+
+    ColorSensor color;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        VuforiaLocalizer.Parameters parms = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
+        parms.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parms.vuforiaLicenseKey = "AbYPrgD/////AAAAGbvKMH3NcEVFmPLgunQe4K0d1ZQi+afRLxricyooCq+sgY9Yh1j+bBrd0CdDCcoieA6trLCKBzymC515+Ps/FECtXv3+CTW6fg3/3+nvKZ6QA18h/cNZHg5HYHmghlcCgVUmSzOLRvdOpbS4S+0Y/sWGXwFK0PbuGPSN82w8XPDBoRYSWjAf8GXeitmNSlm9n4swrMoYNpMDuWCDjSm1kWnoErjFA9NuNoFzAgO+C/rYzoYjTJRk40ETVcAsahzatRlP7PJCvNNXiBhE6iVR+x7lFlTZ841xifOIOPkfVc54olC5XYe4A5ZmQ6WFD03W5HHdQrnmKPmkgcr1yqXAJ3rLTK8FZK3KVgbxz3Eeqps0";
+        parms.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
+
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parms);
+
+        tracker = vuforia.loadTrackablesFromAsset("FTC_2016-17");
+
+        tracker.activate();
+
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+
+        ModernRoboticsI2cGyro gyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
+
+        gyro.resetZAxisIntegrator();
+
+        color = hardwareMap.colorSensor.get("color");
+        color.enableLed(true);
+
+        while (gyro.isCalibrating() && this.opModeIsActive())
+            Thread.sleep(50);
+
+        MecanumDriveTrain wheels = new MecanumDriveTrain("frontleft", "frontright", "backleft", "backright", this);
+
+        shooter = hardwareMap.dcMotor.get("farriswheel");
+        stopper = hardwareMap.servo.get("stopper");
+
+        stopper.setPosition(.75);
+
+        shooter.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.idle();
+        shooter.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        shooter.setMaxSpeed(960);
+
+        gyro.resetZAxisIntegrator();
+        gyro.calibrate();
+
+        while(gyro.isCalibrating()) {
+            Thread.sleep(50);
+        }
+
+        waitForStart();
+
+        wheels.drive(32, Direction.FORWARD, 1250, this);
+
+        shooter.setPower(.3);
+        Thread.sleep(100);
+        shooter.setMaxSpeed(960);
+        shooter.setPower(1);
+
+        wheels.drive(45, Direction.FORWARD, 1300, this);
+
+        wheels.turnWithGyro(90, .3, TurnDirection.LEFT, gyro, this);
+
+        wheels.drive(65, Direction.FORWARD, 1300, this);
+    }
+
+}
+
+}
