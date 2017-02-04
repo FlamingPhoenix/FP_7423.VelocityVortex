f31dcc64d20757f48fbe426bd06081174891f1e4
HwaA18 <HwaA18@gmail.com>
2017-02-03 19:47:43 -0500

    modified autonomous far

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedFar.java
@@ -39,19 +39,6 @@ public class RedFar extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        VuforiaLocalizer.Parameters parms = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
-        parms.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        parms.vuforiaLicenseKey = "AbYPrgD/////AAAAGbvKMH3NcEVFmPLgunQe4K0d1ZQi+afRLxricyooCq+sgY9Yh1j+bBrd0CdDCcoieA6trLCKBzymC515+Ps/FECtXv3+CTW6fg3/3+nvKZ6QA18h/cNZHg5HYHmghlcCgVUmSzOLRvdOpbS4S+0Y/sWGXwFK0PbuGPSN82w8XPDBoRYSWjAf8GXeitmNSlm9n4swrMoYNpMDuWCDjSm1kWnoErjFA9NuNoFzAgO+C/rYzoYjTJRk40ETVcAsahzatRlP7PJCvNNXiBhE6iVR+x7lFlTZ841xifOIOPkfVc54olC5XYe4A5ZmQ6WFD03W5HHdQrnmKPmkgcr1yqXAJ3rLTK8FZK3KVgbxz3Eeqps0";
-        parms.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
-
-        this.vuforia = ClassFactory.createVuforiaLocalizer(parms);
-
-        tracker = vuforia.loadTrackablesFromAsset("FTC_2016-17");
-
-        tracker.activate();
-
-        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
-
         gyro = (ModernRoboticsI2cGyro) hardwareMap.gyroSensor.get("gyro");
 
         gyro.resetZAxisIntegrator();
